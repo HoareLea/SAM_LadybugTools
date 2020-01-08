17bdb75843204ed326699d07ef134707cf88d9e5
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-01-08 10:50:54 +0000

    Update on Grasshopper References

--- Grasshopper/SAM_LadybugTools.Geometry.Grasshopper/ToLadybug/Point3D.py
+++ Grasshopper/SAM_LadybugTools.Geometry.Grasshopper/ToLadybug/Point3D.py
@@ -5,3 +5,21 @@ from __future__ import division
 from .pointvector import Point3D  as LB_Point3D
 from SAM.Geometry.Spatial import Point3D as SAM_Point3D
 
+def Convert(lb_Point3D):
+	sam_Point3D = SAM_Point3D(lb_Point3D.x, lb_Point3D.y, lb_Point3D.z)
+	return sam_Point3D
+
+
+
+from .pointvector import Point3D  as LB_Point3D
+from SAM.Geometry.Spatial import Point3D as SAM_Point3D
+
+def Convert(lb_Point3D):
+	sam_Point3D = SAM_Point3D(lb_Point3D.x, lb_Point3D.y, lb_Point3D.z)
+	return sam_Point3D
+
+
+from .pointvector import Point3D
+
+a = Point3D(x, y, z)
+
