e4e8d53eb20ff53d1f6e8a304589f28b61074075
Michal Dengusiak <tassimulation@gmail.com>
2020-02-10 12:35:58 +0000

    test WIP

--- SAM_LadybugTools/SAM_LadybugTools.Core/Class1.cs
+++ SAM_LadybugTools/SAM_LadybugTools.Core/Class1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HoneybeeDotNet;
 
 namespace SAM.Core.LadybugTools
 {
     public class Class1
     {
+        Face.FaceTypeEnum()
     }
 }
