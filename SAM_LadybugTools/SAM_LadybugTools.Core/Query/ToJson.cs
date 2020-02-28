cb1ebe4fc042fa3e2cf7ae0d934f80891a54e2cc
Michal Dengusiak <tassimulation@gmail.com>
2020-02-28 11:59:33 +0000

    WIP Honeybee To JSON

---
+++ SAM_LadybugTools/SAM_LadybugTools.Core/Query/ToJson.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace SAM.Core.LadybugTools
+{
+    public static partial class Query
+    {
+        public static string ToJson(this object @object)
+        {
+            if (@object == null)
+                return null;
+
+            MethodInfo methodInfo = @object.GetType().GetMethod("ToJson", new Type[] { });
+
+            List<string> names = new List<string>();
+            foreach (MethodInfo methodInfo_Temp in @object.GetType().GetRuntimeMethods())
+            {
+                names.Add(methodInfo_Temp.Name);
+
+                if (!methodInfo_Temp.Name.Equals("ToJson"))
+                    continue;
+
+                ParameterInfo[] parameterInfos = methodInfo_Temp.GetParameters();
+                if (parameterInfos != null && parameterInfos.Length > 0)
+                {
+                    foreach (ParameterInfo parameterInfo in parameterInfos)
+                    {
+                    }
+                }
+            }
+
+            if (methodInfo == null)
+                return null;
+
+            return methodInfo.Invoke(@object, new object[] { }) as string;
+
+        }
+    }
+}
+
