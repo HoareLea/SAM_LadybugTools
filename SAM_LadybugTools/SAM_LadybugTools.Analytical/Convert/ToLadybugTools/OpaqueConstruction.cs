f7aaf8ab0c1d306fb921c40a0646e6d44c4ea120
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-09-21 10:10:29 +0100

    Construction and ApertureConstruction Conversion Added

---
+++ SAM_LadybugTools/SAM_LadybugTools.Analytical/Convert/ToLadybugTools/OpaqueConstruction.cs
@@ -0,0 +1,52 @@
+﻿using HoneybeeSchema;
+using SAM.Core;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.LadybugTools
+{
+    public static partial class Convert
+    {
+        public static OpaqueConstruction OpaqueConstruction(this Construction construction)
+        {
+            if (construction == null)
+                return null;
+
+            List<ConstructionLayer> constructionLayers = construction.ConstructionLayers;
+            if (constructionLayers == null || constructionLayers.Count == 0)
+                return null;
+
+            OpaqueConstruction opaqueConstruction = new OpaqueConstruction(construction.Name, constructionLayers.ConvertAll(x => x.Name), null, construction.Name);
+            return opaqueConstruction;
+        }
+
+        public static OpaqueConstruction OpaqueConstruction(this Construction construction, MaterialLibrary materialLibrary)
+        {
+            if (construction == null || materialLibrary == null)
+                return null;
+
+            List<ConstructionLayer> constructionLayers = construction.ConstructionLayers;
+            if (constructionLayers == null || constructionLayers.Count == 0)
+                return null;
+
+            List<AnyOf<EnergyMaterial, EnergyMaterialNoMass>> materials = new List<AnyOf<EnergyMaterial, EnergyMaterialNoMass>>();
+            foreach (ConstructionLayer constructionLayer in constructionLayers)
+            {
+                IMaterial material = materialLibrary.GetMaterial(constructionLayer.Name);
+                if (material == null)
+                {
+                    materials.Add(null);
+                    continue;
+                }
+
+                if (material is GasMaterial)
+                    materials.Add(((GasMaterial)material).ToLadybugTools());
+                else if (material is OpaqueMaterial)
+                    materials.Add(((OpaqueMaterial)material).ToLadybugTools());
+            }
+
+            OpaqueConstruction opaqueConstruction = new OpaqueConstruction(construction.Name, constructionLayers.ConvertAll(x => x.Name), materials, construction.Name);
+            return opaqueConstruction;
+
+        }
+    }
+}
---
+++ SAM_LadybugTools/SAM_LadybugTools.Analytical/Convert/ToLadybugTools/WindowConstruction.cs
@@ -0,0 +1,61 @@
+﻿using HoneybeeSchema;
+using SAM.Core;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.LadybugTools
+{
+    public static partial class Convert
+    {
+        public static WindowConstruction ToLadybugTools_WindowConstruction(this ApertureConstruction apertureConstruction)
+        {
+            if (apertureConstruction == null)
+                return null;
+
+            Construction paneConstruction = apertureConstruction.PaneConstruction;
+            if (paneConstruction == null)
+                return null;
+
+            List<ConstructionLayer> constructionLayers = paneConstruction.ConstructionLayers;
+            if (constructionLayers == null)
+                return null;
+
+            WindowConstruction result = new WindowConstruction(apertureConstruction.Name, constructionLayers.ConvertAll(x => x.Name), null, apertureConstruction.Name);
+
+            return result;
+        }
+
+        public static WindowConstruction ToLadybugTools_WindowConstruction(this ApertureConstruction apertureConstruction, MaterialLibrary materialLibrary)
+        {
+            if (apertureConstruction == null)
+                return null;
+
+            Construction paneConstruction = apertureConstruction.PaneConstruction;
+            if (paneConstruction == null)
+                return null;
+
+            List<ConstructionLayer> constructionLayers = paneConstruction.ConstructionLayers;
+            if (constructionLayers == null)
+                return null;
+
+            List<AnyOf<EnergyMaterial, EnergyMaterialNoMass>> materials = new List<AnyOf<EnergyMaterial, EnergyMaterialNoMass>>();
+            foreach (ConstructionLayer constructionLayer in constructionLayers)
+            {
+                IMaterial material = materialLibrary.GetMaterial(constructionLayer.Name);
+                if (material == null)
+                {
+                    materials.Add(null);
+                    continue;
+                }
+
+                if (material is GasMaterial)
+                    materials.Add(((GasMaterial)material).ToLadybugTools());
+                else if (material is OpaqueMaterial)
+                    materials.Add(((OpaqueMaterial)material).ToLadybugTools());
+            }
+
+            WindowConstruction result = new WindowConstruction(apertureConstruction.Name, constructionLayers.ConvertAll(x => x.Name), materials, apertureConstruction.Name);
+
+            return result;
+        }
+    }
+}
