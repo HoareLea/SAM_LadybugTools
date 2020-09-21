015c7f506214c0cd77ce485e9693d78dd3ef675e
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-09-21 10:23:23 +0100

    Naming Convention Fix and ApertureConstruction Implementation

--- SAM_LadybugTools/SAM_LadybugTools.Analytical/Convert/ToLadybugTools/OpaqueConstruction.cs
+++ SAM_LadybugTools/SAM_LadybugTools.Analytical/Convert/ToLadybugTools/OpaqueConstruction.cs
@@ -6,7 +6,7 @@ namespace SAM.Analytical.LadybugTools
 {
     public static partial class Convert
     {
-        public static OpaqueConstruction OpaqueConstruction(this Construction construction)
+        public static OpaqueConstruction ToLadybugTools(this Construction construction)
         {
             if (construction == null)
                 return null;
@@ -19,7 +19,7 @@ namespace SAM.Analytical.LadybugTools
             return opaqueConstruction;
         }
 
-        public static OpaqueConstruction OpaqueConstruction(this Construction construction, MaterialLibrary materialLibrary)
+        public static OpaqueConstruction ToLadybugTools(this Construction construction, MaterialLibrary materialLibrary)
         {
             if (construction == null || materialLibrary == null)
                 return null;
@@ -45,6 +45,57 @@ namespace SAM.Analytical.LadybugTools
             }
 
             OpaqueConstruction opaqueConstruction = new OpaqueConstruction(construction.Name, constructionLayers.ConvertAll(x => x.Name), materials, construction.Name);
+            return opaqueConstruction;
+
+        }
+
+        public static OpaqueConstruction ToLadybugTools(this ApertureConstruction apertureConstruction)
+        {
+            if (apertureConstruction == null)
+                return null;
+
+            Construction frameConstruction = apertureConstruction.FrameConstruction;
+            if (frameConstruction == null)
+                return null;
+
+            List<ConstructionLayer> constructionLayers = frameConstruction.ConstructionLayers;
+            if (constructionLayers == null || constructionLayers.Count == 0)
+                return null;
+
+            OpaqueConstruction opaqueConstruction = new OpaqueConstruction(apertureConstruction.Name, constructionLayers.ConvertAll(x => x.Name), null, apertureConstruction.Name);
+            return opaqueConstruction;
+        }
+
+        public static OpaqueConstruction ToLadybugTools(this ApertureConstruction apertureConstruction, MaterialLibrary materialLibrary)
+        {
+            if (apertureConstruction == null || materialLibrary == null)
+                return null;
+
+            Construction frameConstruction = apertureConstruction.FrameConstruction;
+            if (frameConstruction == null)
+                return null;
+
+            List<ConstructionLayer> constructionLayers = frameConstruction.ConstructionLayers;
+            if (constructionLayers == null || constructionLayers.Count == 0)
+                return null;
+
+            List<AnyOf<EnergyMaterial, EnergyMaterialNoMass>> materials = new List<AnyOf<EnergyMaterial, EnergyMaterialNoMass>>();
+            foreach (ConstructionLayer constructionLayer in constructionLayers)
+            {
+                IMaterial material = materialLibrary.GetMaterial(constructionLayer.Name);
+                if (material == null)
+                {
+                    materials.Add(null);
+                    continue;
+                }
+
+                if (material is GasMaterial)
+                    materials.Add(((GasMaterial)material).ToLadybugTools());
+                else if (material is OpaqueMaterial)
+                    materials.Add(((OpaqueMaterial)material).ToLadybugTools());
+            }
+
+            OpaqueConstruction opaqueConstruction = new OpaqueConstruction(apertureConstruction.Name, constructionLayers.ConvertAll(x => x.Name), materials, apertureConstruction.Name);
             return opaqueConstruction;
 
         }
--- SAM_LadybugTools/SAM_LadybugTools.Analytical/Convert/ToLadybugTools/WindowConstruction.cs
+++ SAM_LadybugTools/SAM_LadybugTools.Analytical/Convert/ToLadybugTools/WindowConstruction.cs
@@ -37,7 +37,7 @@ namespace SAM.Analytical.LadybugTools
             if (constructionLayers == null)
                 return null;
 
-            List<AnyOf<EnergyMaterial, EnergyMaterialNoMass>> materials = new List<AnyOf<EnergyMaterial, EnergyMaterialNoMass>>();
+            List<AnyOf<EnergyWindowMaterialSimpleGlazSys, EnergyWindowMaterialGlazing, EnergyWindowMaterialGas, EnergyWindowMaterialGasCustom, EnergyWindowMaterialGasMixture>> materials = new List<AnyOf<EnergyWindowMaterialSimpleGlazSys, EnergyWindowMaterialGlazing, EnergyWindowMaterialGas, EnergyWindowMaterialGasCustom, EnergyWindowMaterialGasMixture>>();
             foreach (ConstructionLayer constructionLayer in constructionLayers)
             {
                 IMaterial material = materialLibrary.GetMaterial(constructionLayer.Name);
@@ -48,9 +48,9 @@ namespace SAM.Analytical.LadybugTools
                 }
 
                 if (material is GasMaterial)
-                    materials.Add(((GasMaterial)material).ToLadybugTools());
-                else if (material is OpaqueMaterial)
-                    materials.Add(((OpaqueMaterial)material).ToLadybugTools());
+                    materials.Add(((GasMaterial)material).ToLadybugTools_EnergyWindowMaterialGas());
+                else if (material is TransparentMaterial)
+                    materials.Add(((TransparentMaterial)material).ToLadybugTools());
             }
 
             WindowConstruction result = new WindowConstruction(apertureConstruction.Name, constructionLayers.ConvertAll(x => x.Name), materials, apertureConstruction.Name);
