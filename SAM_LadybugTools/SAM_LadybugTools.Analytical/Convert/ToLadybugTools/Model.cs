b8f755c15035aa677bca40e6dc484c1d986faeb0
Michal Dengusiak <tassimulation@gmail.com>
2020-09-30 09:26:30 +0100

    update to 1.39.0

--- SAM_LadybugTools/SAM_LadybugTools.Analytical/Convert/ToLadybugTools/Model.cs
+++ SAM_LadybugTools/SAM_LadybugTools.Analytical/Convert/ToLadybugTools/Model.cs
@@ -271,7 +271,7 @@ namespace SAM.Analytical.LadybugTools
 
             ModelProperties modelProperties = new ModelProperties(modelEnergyProperties);
 
-            Model model = new Model(uniqueName, modelProperties, adjacencyCluster.Name, null, "1.38.3", rooms, faces_Orphaned, shades);
+            Model model = new Model(uniqueName, modelProperties, adjacencyCluster.Name, null, "1.39.0", rooms, faces_Orphaned, shades);
             //model.AngleTolerance = Core.Tolerance.Angle;
             model.AngleTolerance = 2;
             model.Tolerance = Core.Tolerance.MacroDistance;
