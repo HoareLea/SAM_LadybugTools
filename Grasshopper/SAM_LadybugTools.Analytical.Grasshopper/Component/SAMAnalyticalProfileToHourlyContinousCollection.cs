5e4c7f5dd01f2051585d00cc5f34d60230188b14
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-02-01 16:44:26 +0000

    ProfileToHourlyContinousCollection Grasshopper Component Added

---
+++ Grasshopper/SAM_LadybugTools.Analytical.Grasshopper/Component/SAMAnalyticalProfileToHourlyContinousCollection.cs
@@ -0,0 +1,89 @@
+﻿using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.LadybugTools.Properties;
+using SAM.Core.Grasshopper;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Grasshopper.LadybugTools
+{
+    public class SAMAnalyticalProfileToHourlyContinousCollection : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("b87cf215-90e3-4345-b037-ccedecd959bc");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Honeybee;
+
+        public override GH_Exposure Exposure => GH_Exposure.secondary;
+
+        /// <summary>
+        /// Initializes a new instance of the SAMGeometryByGHGeometry class.
+        /// </summary>
+        public SAMAnalyticalProfileToHourlyContinousCollection()
+          : base("SAMAnalytical.ProfileToHourlyContinousCollection", "SAMAnalytical.ProfileToHourlyContinousCollection",
+              "SAM Analytical Profile to HourlyContinousCollection",
+              "SAM", "LadybugTools")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new GooProfileParam() { Name = "_profile", NickName = "_profile", Description = "SAM Analytical Profile", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_String() { Name = "HourlyContinousCollection", NickName = "HourlyContinousCollection", Description = "HourlyContinousCollection", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index = -1;
+
+            Profile profile = null;
+
+            index = Params.IndexOfInputParam("_profile");
+            if(index == -1 || !dataAccess.GetData(index, ref profile) || profile == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            index = Params.IndexOfOutputParam("HourlyContinousCollection");
+            if(index != -1)
+            {
+                string hourlyContinousCollection = Analytical.LadybugTools.Convert.ToLadybugTools_HourlyContinousCollection(profile);
+                dataAccess.SetData(index, hourlyContinousCollection);
+            }
+        }
+    }
+}
---
+++ SAM_LadybugTools/SAM_LadybugTools.Analytical/Convert/ToLadybugTools/HourlyContinousCollection.cs
@@ -0,0 +1,111 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.LadybugTools
+{
+    public static partial class Convert
+    {
+        public static string ToLadybugTools_HourlyContinousCollection(this Profile profile, Dictionary<string, object> metadata = null)
+        {
+            if (profile == null)
+                return null;
+
+            string unit = null;
+            string name = profile.Name;
+            string dataType = "GenericType";
+            switch(profile.ProfileGroup)
+            {
+                case ProfileGroup.Gain:
+                    unit = "W";
+                    break;
+                
+                case ProfileGroup.Humidistat:
+                    unit = "-";
+                    break;
+                
+                case ProfileGroup.Thermostat:
+                    unit = "C";
+                    break;
+
+                default:
+                    return null;
+            }
+
+            double[] values = null;
+
+            int endHour = 23;
+            int endDay = -1;
+            int endMonth = -1;
+
+            int count = profile.Count;
+            if (count <= 24)
+            {
+                values = profile.GetDailyValues();
+                endDay = 1;
+                endMonth = 1;
+            }
+            else
+            {
+                values = profile.GetYearlyValues();
+                endDay = 31;
+                endMonth = 12;
+            }
+                
+
+            if (values == null)
+                return null;
+
+            return ToLadybugTools_HourlyContinousCollection(values, unit, name, dataType, 0, 1, 1, endHour, endDay, endMonth, 1, false, metadata);
+
+        }
+
+        public static string ToLadybugTools_HourlyContinousCollection(double[] values, string unit, string name, string dataType, int startHour, int startDay,int startMonth, int endHour, int endDay, int endMonth, int timestep, bool leapYear, Dictionary<string, object> metadata = null)
+        {
+            if (values == null)
+                return null;
+
+            JObject result = new JObject();
+            result.Add("values", new JArray(values));
+
+            JObject jObject_Header = new JObject();
+            
+            if(metadata != null)
+            {
+                JObject jObject_Metadata = new JObject();
+                foreach (KeyValuePair<string, object> keyValuePair in metadata)
+                    jObject_Metadata.Add(keyValuePair.Key, keyValuePair.Value as dynamic);
+
+                jObject_Header.Add("metadata", jObject_Metadata);
+            }
+
+            JObject jObject_AnalysisPeriod = new JObject();
+            jObject_AnalysisPeriod.Add("st_day", startDay);
+            jObject_AnalysisPeriod.Add("end_month", endMonth);
+            jObject_AnalysisPeriod.Add("st_hour", startHour);
+            jObject_AnalysisPeriod.Add("end_hour", endHour);
+            jObject_AnalysisPeriod.Add("timestep", timestep);
+            jObject_AnalysisPeriod.Add("is_leap_year", leapYear);
+            jObject_AnalysisPeriod.Add("st_month", startMonth);
+            jObject_AnalysisPeriod.Add("end_day", endDay);
+            jObject_AnalysisPeriod.Add("type", "AnalysisPeriod");
+
+            jObject_Header.Add("analysis_period", jObject_AnalysisPeriod);
+
+            jObject_Header.Add("unit", unit);
+            jObject_Header.Add("type", "Header");
+
+            JObject jObject_DataType = new JObject();
+            jObject_DataType.Add("base_unit", "C");
+            jObject_DataType.Add("name", name);
+            jObject_DataType.Add("type", "DataTypeBase");
+            jObject_DataType.Add("data_type", dataType);
+
+            jObject_Header.Add("data_type", jObject_DataType);
+
+            result.Add("header", jObject_Header);
+            result.Add("type", "HourlyContinuousCollection");
+
+            return result.ToString();
+        }
+    }
+}
