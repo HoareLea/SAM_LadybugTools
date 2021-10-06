a61bab368864c445a827b0dc7a750fca074755c3
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-10-06 15:27:37 +0100

    ToJson Conversion Updated (WIP)

--- SAM_LadybugTools/SAM_LadybugTools.Core/Query/ToJson.cs
+++ SAM_LadybugTools/SAM_LadybugTools.Core/Query/ToJson.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Reflection;
+using System.Text;
+using System.Text.Json;
 
 namespace SAM.Core.LadybugTools
 {
@@ -25,17 +28,33 @@ namespace SAM.Core.LadybugTools
             {
                 //TODO:  Finish Implementation
 
-                foreach (dynamic item in @dynamic.items())
+                JsonWriterOptions jsonWriterOptions = new JsonWriterOptions
                 {
-                    string name = item[0];
-                    object value = item[1];
-                    if(value is string)
-                    {
+                    Indented = true
+                };
 
+                try
+                {
+                    string json = null;
+
+                    using (MemoryStream memoryStream = new MemoryStream())
+                    {
+                        using (Utf8JsonWriter utf8JsonWriter = new Utf8JsonWriter(memoryStream, jsonWriterOptions))
+                        {
+                            ToJson(@dynamic, utf8JsonWriter);
+
+                            utf8JsonWriter.Flush();
+                        }
+
+                        json = Encoding.UTF8.GetString(memoryStream.ToArray());
                     }
-                    Type type = item.GetType();
+                }
+                catch(Exception exception)
+                {
 
                 }
+                
+
             }
 
             MethodInfo methodInfo = @object.GetType().GetMethod("ToJson", new Type[] { });
@@ -64,5 +83,49 @@ namespace SAM.Core.LadybugTools
             
             return null;
         }
+
+        private static void ToJson(dynamic @dynamic, Utf8JsonWriter utf8JsonWriter)
+        {
+            if(dynamic == null || utf8JsonWriter == null)
+            {
+                return;
+            }
+
+            utf8JsonWriter.WriteStartObject();
+
+            foreach (dynamic item in @dynamic.items())
+            {
+                string name = item[0];
+                object value = item[1];
+                if (value == null)
+                {
+                    utf8JsonWriter.WriteNull(name);
+                }
+
+                if(Core.Query.IsNumeric(value))
+                {
+                    utf8JsonWriter.WriteNumber(name, value as dynamic);
+                }
+                else if(value is string)
+                {
+                    utf8JsonWriter.WriteString(name, value as dynamic);
+                }
+                else
+                {
+                    try
+                    {
+                        //ToJson(item, utf8JsonWriter);
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+
+            utf8JsonWriter.WriteEndObject();
+        }
+
+
     }
 }
