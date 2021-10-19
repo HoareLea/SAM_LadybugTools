2f1ea7f6f3b729707b6b30a56b4965d252f8a1de
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-10-19 11:28:49 +0100

    Space Volume and Location Point Calculated

---
+++ SAM_LadybugTools/SAM_LadybugTools.Geometry/Query/Shell.cs
@@ -0,0 +1,36 @@
+﻿using SAM.Geometry.Spatial;
+using System.Collections.Generic;
+
+namespace SAM.Geometry.LadybugTools
+{
+    public static partial class Query
+    {
+        public static Shell Shell(this HoneybeeSchema.Room room)
+        {
+            if(room == null)
+            {
+                return null;
+            }
+
+            List<HoneybeeSchema.Face> faces = room.Faces;
+            if (faces == null && faces.Count < 3)
+            {
+                return null;
+            }
+
+            List<Face3D> face3Ds = new List<Face3D>();
+            foreach (HoneybeeSchema.Face face in room.Faces)
+            {
+                Face3D face3D = Convert.ToSAM(face.Geometry);
+                if (face3D == null)
+                {
+                    return null;
+                }
+
+                face3Ds.Add(face3D);
+            }
+
+            return new Shell(face3Ds);
+        }
+    }
+}
