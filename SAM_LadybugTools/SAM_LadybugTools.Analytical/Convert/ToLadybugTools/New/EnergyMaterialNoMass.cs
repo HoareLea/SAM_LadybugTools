d885b41a390c6045166ad5beedafe5835230997d
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-10-27 09:46:25 +0100

    EnergyMaterialNoMass Create Method Added

---
+++ SAM_LadybugTools/SAM_LadybugTools.Analytical/Convert/ToLadybugTools/New/EnergyMaterialNoMass.cs
@@ -0,0 +1,16 @@
+﻿using HoneybeeSchema;
+using SAM.Core;
+
+namespace SAM.Analytical.LadybugTools
+{
+    public static partial class Convert
+    {
+        public static EnergyMaterialNoMass ToLadybugTools(this GasMaterial gasMaterial)
+        {
+            if (gasMaterial == null || string.IsNullOrEmpty(gasMaterial.Name))
+                return null;
+
+            return new EnergyMaterialNoMass(identifier: gasMaterial.Name, rValue: gasMaterial.GetValue<double>(GasMaterialParameter.HeatTransferCoefficient), displayName: gasMaterial.DisplayName);
+        }
+    }
+}
