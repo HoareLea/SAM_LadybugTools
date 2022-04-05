435bc0c54b8c0bcb2a02d8a54ab5e0f5f669bc29
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-04-05 14:44:13 +0100

    Missing MaterialLayers Query Added

--- SAM_LadybugTools/SAM_LadybugTools.Analytical/Query/ConstructionLayers.cs
+++ SAM_LadybugTools/SAM_LadybugTools.Analytical/Query/ConstructionLayers.cs
@@ -10,16 +10,31 @@ namespace SAM.Analytical.LadybugTools
             if (materialLibrary == null || names == null)
                 return null;
 
-            List<ConstructionLayer> result = new List<ConstructionLayer>();
+            List<IMaterial> materials = new List<IMaterial>();
             foreach (string name in names)
             {
-                
                 Material material = materialLibrary.GetMaterials()?.Find(x => x is Material && name == x.Name) as Material;
                 if (material == null)
                 {
                     continue;
                 }
 
+                materials.Add(material);
+            }
+
+            return ConstructionLayers(materials);
+        }
+
+        public static List<ConstructionLayer> ConstructionLayers(this IEnumerable<IMaterial> materials)
+        {
+            if(materials == null)
+            {
+                return null;
+            }
+
+            List<ConstructionLayer> result = new List<ConstructionLayer>();
+            foreach (IMaterial material in materials)
+            {
                 double thickness = material.GetValue<double>(Core.MaterialParameter.DefaultThickness);
                 result.Add(new ConstructionLayer(material.Name, thickness));
             }
